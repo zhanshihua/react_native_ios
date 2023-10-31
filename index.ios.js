16dca71983899678410843e89b65085798dfb768
zhanshihua <zhanshihua@able-elec.com>
2023-10-31 16:32:24 +0800

    fix: web

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 import TrackPlayer from 'react-native-track-player';
 TrackPlayer.registerPlaybackService(() => require('./service'));
---
+++ src/index.web.js
@@ -0,0 +1,13 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from '../app.json';
+import TrackPlayer from 'react-native-track-player';
+TrackPlayer.registerPlaybackService(() => require('../service'));
+AppRegistry.registerComponent(appName, () => App);
+AppRegistry.runApplication(appName, {
+  rootTag: document.getElementById('root'),
+});
